a0298972fbf0e6d656f56f4cac3fd83981da31a9
Maria Ionova <mionova@gmail.com>
2017-06-08 13:55:07 -0700

    Checkpoint 29 jQuery Collection: completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,9 +1,10 @@
- var collectionItemTemplate =
+ var buildCollectionItemTemplate = function() {
+     var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
    + '    <p>'
-   + '      <a class="album-name" href="album.html"> The Colors </a>'
+   + '      <a class="album-name" href="album.html"> The Colors      </a>'
    + '      <br/>'
    + '      <a href="album.html"> Pablo Picasso </a>'
    + '      <br/>'
@@ -13,14 +14,17 @@
    + '  </div>'
    + '</div>'
    ;
- window.onload = function() {
-     // #1
-     var collectionContainer = document.getElementsByClassName('album-covers')[0];
-     // #2
-     collectionContainer.innerHTML = '';
  
-     // #3
+     return $(template);
+ };  
+
+ $(window).load(function() {
+
+     var $collectionContainer = $('.album-covers');
+     $collectionContainer.empty();
+ 
      for (var i = 0; i < 12; i++) {
-         collectionContainer.innerHTML += collectionItemTemplate;
+         var $newThumbnail = buildCollectionItemTemplate();
+         $collectionContainer.append($newThumbnail);
      }
- }
+ });
